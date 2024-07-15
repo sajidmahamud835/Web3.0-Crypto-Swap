aca3c5c5b9cbb49f2d2856f23906cebf5852771b
Sajid Mahamud <83598348+sajidmahamud835@users.noreply.github.com>
2024-07-15 08:41:07 +0000

    removing stuff for debuggin

--- client/src/app/lib/constants.js
+++
@@ -1,4 +0,0 @@
-import abi from './Transactions.json'
-
-export const contractABI = abi.abi
-export const contractAddress = '0xa0F2EC2FBb680b56E7Eb5bC00EDD9d326A75Ed72'
--- client/src/app/lib/sanityClient.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@sanity/client'
-
-export const client = createClient({
-  projectId: 'nw03is8d',
-  dataset: 'production',
-  apiVersion: 'v1', // use current UTC date - see "specifying API version"!
-  useCdn: false // `false` if you want to ensure fresh data
-})
