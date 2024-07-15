df1caaccf2ca5d1ee757b7138de613feb2c63506
Sajid Mahamud <83598348+sajidmahamud835@users.noreply.github.com>
2024-07-15 05:19:38 +0000

    hover effect added

--- client/src/app/components/FollowUs.js
+++ client/src/app/components/FollowUs.js
@@ -1,7 +1,9 @@
+import React from 'react';
+
 export default function FollowUs() {
   return (
     <div className="min-h-[300px] flex flex-col justify-center items-center bg-gray-100 py-12 dark:bg-gray-900 px-4 sm:px-6 lg:px-8">
-      <div className="text-center">
+      <div className="text-center space-y-4">
         <h1 className="text-4xl md:text-6xl font-bold text-gray-800 mb-4">
           Follow <span className="text-blue-500">@CryptoSwap</span>
         </h1>
@@ -11,7 +13,7 @@ export default function FollowUs() {
 
         <button
           type="button"
-          class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-lg px-8 py-3 text-center me-2 mb-2 "
+          className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:from-blue-600 hover:to-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-lg px-8 py-3 transition duration-300 ease-in-out transform hover:scale-105"
         >
           Go to @CryptoSwap
         </button>
