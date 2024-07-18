0d1b134047470cf725a847a103a08779bb3be787
Sajid Mahamud <83598348+sajidmahamud835@users.noreply.github.com>
2024-07-18 07:04:06 +0000

    error handeling

--- client/src/pages/lib/db.js
+++ client/src/pages/lib/db.js
@@ -13,17 +13,23 @@ if (!process.env.MONGODB_URI) {
   throw new Error('Please add your Mongo URI to .env.local');
 }
 
-if (process.env.NODE_ENV === 'development') {
-  // In development mode, use a global variable so the client is not recreated
-  if (!global._mongoClientPromise) {
+// Ensure this code is only executed on the server side
+if (typeof window === 'undefined') {
+  if (process.env.NODE_ENV === 'development') {
+    // In development mode, use a global variable so the client is not recreated
+    if (!global._mongoClientPromise) {
+      client = new MongoClient(uri, options);
+      global._mongoClientPromise = client.connect();
+    }
+    clientPromise = global._mongoClientPromise;
+  } else {
+    // In production mode, it's best to not use a global variable
     client = new MongoClient(uri, options);
-    global._mongoClientPromise = client.connect();
+    clientPromise = client.connect();
   }
-  clientPromise = global._mongoClientPromise;
 } else {
-  // In production mode, it's best to not use a global variable
-  client = new MongoClient(uri, options);
-  clientPromise = client.connect();
+  // If this code somehow runs on the client, set clientPromise to null
+  clientPromise = null;
 }
 
 export default clientPromise;
