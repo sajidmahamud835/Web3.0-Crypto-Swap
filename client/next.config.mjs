c00cb4638fc50346a7bfd8bd8ae3b22abdd5d9ef
Sajid Mahamud <83598348+sajidmahamud835@users.noreply.github.com>
2024-07-18 07:17:21 +0000

    error handeling

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -4,11 +4,20 @@ const nextConfig = {
         ZeroX_API_KEY: process.env.ZeroX_API_KEY,
         MONGODB_URI: process.env.MONGODB_URI
       },
-      webpack: (config) => {
-        config.resolve.fallback = { fs: false, net: false, tls: false };
+      webpack: (config, { isServer }) => {
+        if (!isServer) {
+          config.resolve.fallback = {
+            ...config.resolve.fallback,
+            child_process: false,
+            dns: false,
+            fs: false,
+            net: false,
+            tls: false,
+            'timers/promises': false,
+          };
+        }
         return config;
       },
-
 };
 
 export default nextConfig;
