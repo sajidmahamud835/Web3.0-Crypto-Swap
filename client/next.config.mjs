4f0f22878a7c12e28c5f247871c57ca94faa0cfe
Sajid Mahamud <83598348+sajidmahamud835@users.noreply.github.com>
2024-07-18 07:34:41 +0000

    fixing build issue

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,23 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        ZeroX_API_KEY: process.env.ZeroX_API_KEY,
-        MONGODB_URI: process.env.MONGODB_URI
-      },
-      webpack: (config, { isServer }) => {
-        if (!isServer) {
-          config.resolve.fallback = {
-            ...config.resolve.fallback,
-            child_process: false,
-            dns: false,
-            fs: false,
-            net: false,
-            tls: false,
-            'timers/promises': false,
-          };
-        }
-        return config;
-      },
+  env: {
+      ZeroX_API_KEY: process.env.ZeroX_API_KEY,
+      MONGODB_URI: process.env.MONGODB_URI
+    },
+    webpack: (config, { isServer }) => {
+      config.module.rules.push({
+        test: /\.node$/,
+        use: 'node-loader',
+      });
+      config.resolve.fallback = { fs: false, net: false, tls: false };
+      if (!isServer) {
+        config.resolve.fallback = {
+          ...config.resolve.fallback,
+          child_process: false,
+          dns: false,
+          'timers/promises': false,
+        };
+      }
+      return config;
+    },
 };
 
 export default nextConfig;
